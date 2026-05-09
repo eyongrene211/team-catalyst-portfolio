98f6dfe1843bf5261f8e06e65b39bc9b19bb124a
eyongrene211 <eyongrene211@gmail.com>
2026-05-09 19:43:14 +0100

    fix view more functionality on project-section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 const toggleContainer = document.getElementById('theme-toggle-btn');
 const toggleBtn = document.getElementById('toggle-btn');
 const html = document.documentElement;
+const showProjectsBtn = document.getElementById('more-projects-btn');
+const detailedContent = document.querySelector('.more-projects');
 
 function updateThemeUI() {
     const isDark = html.classList.contains('dark');
@@ -29,3 +31,24 @@ toggleContainer.addEventListener('click', () => {
     updateThemeUI();
     localStorage.setItem('theme', html.classList.contains('dark') ? 'dark' : 'light');
 });
+
+//toggle  'show more' btn 
+showProjectsBtn.addEventListener('click', () => {
+    const isVisible = detailedContent.classList.toggle('visible');
+    if (isVisible) {
+        showProjectsBtn.textContent= 'view less'
+    } else {
+        showProjectsBtn.textContent= 'view more'
+    }
+    // curentBtnState = detailedContent.textContent.trim()
+    console.log('bnt pressed!')
+    // console.log(curentBtnState)
+    // if (curentBtnState == 'view more') {
+    //     detailedContent.style.display = 'block';
+    //     // moreProjects.style.clock = 'blue';
+    //     showProjectsBtn.textContent = ' view less'
+    // } else {
+    //     // moreProjects.style.display = 'none';
+    //     showProjectsBtn.textContent = 'view more'
+    // }
+})
